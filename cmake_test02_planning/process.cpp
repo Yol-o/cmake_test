8e166de191b443d8167847ac1ae09259301a0758
Yolo <ginger_nan@163.com>
2025-02-14 16:02:46 +0800

    [ADD] cmake_test02_planning 版本v0.0.1

---
+++ cmake_test02_planning/process.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "process.h"
+
+void Process::planProcess()
+{
+    std::cout << "this is planning process" << std::endl;
+    my_map.mapInfo();
+    std::cout << "planning sucess!" << std::endl;
+}
